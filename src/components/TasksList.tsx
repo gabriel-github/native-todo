97f9d6039f74786486c58a8f270b35fafd8eabfb
gabriel-github <gabrielferrsantos201@gmail.com>
2021-09-29 19:51:43 -0300

    challenge 2

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -48,33 +48,13 @@ export function TasksList({
       renderItem={({ item, index }) => {
         return (
           <ItemWrapper index={index}>
-            <View>
-              <TouchableOpacity
-                testID={`button-${index}`}
-                activeOpacity={0.7}
-                style={styles.taskButton}
-                onPress={() => toggleTaskDone(item.id)}
-              >
-                <View
-                  testID={`marker-${index}`}
-                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
-                >
-                  {item.done && <Icon name="check" size={12} color="#FFF" />}
-                </View>
-
-                <Text style={item.done ? styles.taskTextDone : styles.taskText}>
-                  {item.title}
-                </Text>
-              </TouchableOpacity>
-            </View>
-
-            <TouchableOpacity
-              testID={`trash-${index}`}
-              style={{ paddingHorizontal: 24 }}
-              onPress={() => removeTask(item.id)}
-            >
-              <Image source={trashIcon} />
-            </TouchableOpacity>
+            <TaskItem
+              index={index}
+              task={item}
+              toggleTaskDone={toggleTaskDone}
+              removeTask={removeTask}
+              editTask={editTask}
+            />
           </ItemWrapper>
         );
       }}
@@ -84,51 +64,3 @@ export function TasksList({
     />
   );
 }
-
-const styles = StyleSheet.create({
-  iconsContainer: {
-    display: "flex",
-  },
-  taskButton: {
-    flex: 1,
-    paddingHorizontal: 24,
-    paddingVertical: 15,
-    marginBottom: 4,
-    borderRadius: 4,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  taskMarker: {
-    height: 16,
-    width: 16,
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: "#B2B2B2",
-    marginRight: 15,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  taskText: {
-    color: "#666",
-    fontFamily: "Inter-Medium",
-  },
-  taskMarkerDone: {
-    height: 16,
-    width: 16,
-    borderRadius: 4,
-    backgroundColor: "#1DB863",
-    marginRight: 15,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  taskTextDone: {
-    color: "#1DB863",
-    textDecorationLine: "line-through",
-    fontFamily: "Inter-Medium",
-  },
-  iconsDivider: {
-    width: 1,
-    height: 24,
-    color: "rgba(196, 196, 196, 0.24)",
-  },
-});
